641ea6f4840c44eff1438b8f4d3b83ba3e698497
NMurphy85 <murphy.nathant@gmail.com>
2026-03-07 20:16:13 -0500

    added fake data to books section dynamic

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Nav from './components/Nav';
 import './index.css';
 import Landing from './components/Landing';
 import Highlights from './components/Highlights';
+import Featured from './components/Featured';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Nav />
       <Landing />
       <Highlights />
+      <Featured />
     </div>
   );
 }
