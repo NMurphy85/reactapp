58c6aa28ce9983b4bca974ac5bf1512665ca9048
NMurphy85 <murphy.nathant@gmail.com>
2026-03-11 21:02:29 -0400

    made booksinfo pages dynamic

--- src/components/ui/Book.jsx
+++ src/components/ui/Book.jsx
@@ -7,13 +7,13 @@ const Book = ({ book }) => {
     const rating = 4
   return (
     <div className="book">
-      <Link to="/books/:id">
+      <Link to ={ `/books/${book.id}`}>
         <figure className="book__img--wrapper">
           <img className="book__img" src={book.url} alt="" />
         </figure>
       </Link>
       <div className="book__title">
-        <Link to="/books/:id" className="book__title--link">
+        <Link to={`/books/${book.id}`} className="book__title--link">
           {book.title}
         </Link>
       </div>
