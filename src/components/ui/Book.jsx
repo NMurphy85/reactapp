f29c75160bf53e0214c5f4301c53fcbf3a80c627
NMurphy85 <murphy.nathant@gmail.com>
2026-03-17 21:16:56 -0400

    added loading state

--- src/components/ui/Book.jsx
+++ src/components/ui/Book.jsx
@@ -1,26 +1,61 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Link } from "react-router-dom";
 import Rating from "./Rating";
 import Price from "./Price";
+
 const Book = ({ book }) => {
-    const rating = 4
+  const [img, setImg] = useState();
+
+  const mountedRef = useRef(true);
+
+  useEffect(() => {
+    const image = new Image();
+    image.src = book.url;
+    image.onload = () => {
+      setTimeout(() => {
+        if (!mountedRef.current) {
+          setImg(image)
+        }
+      }, 300)
+    }
+    return () => {
+      //When the component unmounts
+      mountedRef.current = false;
+    }
+  })
+    
+
   return (
     <div className="book">
-      <Link to ={ `/books/${book.id}`}>
-        <figure className="book__img--wrapper">
-          <img className="book__img" src={book.url} alt="" />
-        </figure>
-      </Link>
-      <div className="book__title">
-        <Link to={`/books/${book.id}`} className="book__title--link">
-          {book.title}
-        </Link>
-      </div>
-      <div className="book__ratings">
-       <Rating rating={book.rating} />
-      </div>
-        <Price salePrice={book.salePrice} originalPrice={book.originalPrice} />
+      {img ? (
+        <>
+          <Link to={`/books/${book.id}`}>
+            <figure className="book__img--wrapper">
+              <img className="book__img" src={img.src} alt="" />
+            </figure>
+          </Link>
+          <div className="book__title">
+            <Link to={`/books/${book.id}`} className="book__title--link">
+              {book.title}
+            </Link>
+          </div>
+          <div className="book__ratings">
+            <Rating rating={book.rating} />
+          </div>
+          <Price
+            salePrice={book.salePrice}
+            originalPrice={book.originalPrice}
+          />
+        </>
+      ) : (
+        <>
+          <div className="book__img--skeleton"></div>
+          <div className="skeleton book--title--skeleton"></div>
+          <div className="skeleton book--rating--skeleton"></div>
+          <div className="skeleton book--price--skeleton"></div>
+        </>
+      )}
     </div>
   );
 };
